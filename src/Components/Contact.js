26936c5b2ad25ded4d67cd5f571a14d560b5581d
Damiano Chintala <damianochintala@gmail.com>
2020-06-04 12:11:47 +0200

    enabled contact form functionality

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,83 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
 import axios from 'axios';
 
-class Contact extends Component {
- constructor(props){
- 	super(props);
- 	this.state={
- 		name: ' ',
- 		subject: ' ',
- 		email: ' ',
- 		message: ' ',
- 		sent: false,
- 		buttonText: 'Send Message'
- 	}
- }
+function Contact3(props){
+	
+	const [serverState, setServerState] = useState({
+	submitting: false,
+	status: null
+	});
+	const handleServerResponse = (ok, msg, form) => {
+	setServerState({
+	submitting: false,
+	status: { ok, msg }
+	});
+	if (ok) {
+	form.reset();
+	}
+	};
+	const handleOnSubmit = e => {
+	e.preventDefault();
+	const form = e.target;
+	setServerState({ submitting: true });
+	axios({
+	method: "post",
+	url: "https://formspree.io/moqkdley",
+	data: new FormData(form)
+	})
+	.then(r => {
+	handleServerResponse(true, "Thanks! Message successfully sent", form);
+	})
+	.catch(r => {
+	handleServerResponse(false, r.response.data.error, form);
+	});
+	};
 
- onSubjectChange(event) {
- 	this.setState({subject: event.target.value})
-   }
 
-onNameChange(event) {
-	this.setState({name: event.target.value})
-  }
-
-  onEmailChange(event) {
-	this.setState({email: event.target.value})
-  }
-
-  onMessageChange(event) {
-	this.setState({message: event.target.value})
-  }
-
-handleSubmit=(e)=>{
-    e.preventDefault();
-    this.setState({
-          buttonText: '...sending'
-      })
-    
-      let data = {
-          name: this.state.name,
-          email: this.state.email,
-          message: this.state.message,
-          subject: this.state.subject
-      }
-      
-      axios.post('API_URI', data)
-      .then( res => {
-          this.setState({ sent: true }, this.resetForm())
-      })
-      .catch( () => {
-        alert('Form Currently not Operational, use other contact ways');
-        this.resetForm();
-      })
-  }
-
-  resetForm=()=>{
-    
-     this.setState({
-     name: ' ', 
-     subject: ' ', 
-     email: ' ', 
-     message: ' ',
-     buttonText: 'Message Sent'
-     })
-  }
- 
-  render() {
-
-    if(this.props.data){
-      var name = this.props.data.name;
-      var street = this.props.data.address.street;
-      var city = this.props.data.address.city;
-      var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
-      var email = this.props.data.email;
-      var message = this.props.data.contactmessage;
-    }
 
     return (
+    
+     
       <section id="contact">
 
          <div className="row section-head">
@@ -90,7 +50,7 @@ handleSubmit=(e)=>{
 
             <div className="ten columns">
 
-                  <p className="lead">{message}</p>
+                  <p className="lead">If you are seeking to hire a Developer ,want someone to collaborate with or for any freelance work, mobile or web applications development, contact me and will get back to you as soon as possible</p>
 
             </div>
 
@@ -99,31 +59,37 @@ handleSubmit=(e)=>{
          <div className="row">
             <div className="eight columns">
 
-               <form action=""  name="contactForm" onSubmit={this.handleSubmit} >
+               <form action=""  name="contactForm" onSubmit={handleOnSubmit} >
+               
 					<fieldset>
 
                   <div>
 						   <label htmlFor="contactName">Name <span className="required">*</span></label>
-						   <input type="text"  size="35" id="contactName" name="contactName" value={this.state.name} onChange={this.onNameChange.bind(this)}/>
+						   <input type="text"  size="35" id="name" name="contactName" />
                   </div>
 
                   <div>
 						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" value={this.state.email} onChange={this.onEmailChange.bind(this)}/>
+						   <input type="email" defaultValue="" size="35" id="email" name="contactEmail" />
                   </div>
 
                   <div>
 						   <label htmlFor="contactSubject">Subject</label>
-						   <input type="text"  size="35" id="contactSubject" name="contactSubject" value={this.state.subject} onChange={this.onSubjectChange.bind(this)}/>
+						   <input type="text"  size="35" id="subject" name="contactSubject" />
                   </div>
 
                   <div>
                      <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                     <textarea cols="50" rows="15" id="contactMessage" name="contactMessage" value={this.state.message} onChange={this.onMessageChange.bind(this)} ></textarea>
+                     <textarea cols="50" rows="15" id="message" name="contactMessage" ></textarea>
                   </div>
 
                   <div>
-                     <button type="submit" className="submit">Submit</button>
+                     <button type="submit" disabled={serverState.submitting}>Submit</button>
+                     {serverState.status && (
+                     <p className={!serverState.status.ok ? "errorMsg" : ""}>
+                     {serverState.status.msg}
+                     </p>
+                     )}
                      <span id="image-loader">
                         <img alt="" src="images/loader.gif" />
                      </span>
@@ -131,29 +97,14 @@ handleSubmit=(e)=>{
 					</fieldset>
 				   </form>
 
-           <div id="message-warning"> Error boy</div>
+           <div id="message-warning"> Error</div>
 				   <div id="message-success">
                   <i className="fa fa-check"></i>Your message was sent, thank you!<br />
 				   </div>
            </div>
-
-
-            <aside className="four columns footer-widgets">
-               <div className="widget widget_contact">
-
-					   <h4>Address and Phone</h4>
-					   <p className="address">
-						   {name}<br />
-						   {street} <br />
-						   {city}, {state} {zip}<br />
-						   <span>{phone}</span>
-					   </p>
-				   </div>
-            </aside>
       </div>
    </section>
     );
   }
-}
 
-export default Contact;
+export default Contact3;
