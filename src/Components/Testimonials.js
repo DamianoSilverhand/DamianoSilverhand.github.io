c162c76863f631efc582b91c9b2bf1949fca99d4
Damiano Chintala <damianochintala@gmail.com>
2025-08-21 11:35:18 +0200

    Update Testimonials.js with modern card layout and improved visual design

--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -5,32 +5,51 @@ class Testimonials extends Component {
 
     if(this.props.data){
       var testimonials = this.props.data.testimonials.map(function(testimonials){
-        return  <li key={testimonials.user}>
-            <blockquote>
-               <p>{testimonials.text}</p>
-               <cite>{testimonials.user}</cite>
-            </blockquote>
-         </li>
+        return (
+          <div key={testimonials.user} className="testimonial-item">
+            <div className="testimonial-content">
+              <div className="quote-icon">
+                <i className="fa fa-quote-left"></i>
+              </div>
+              <p className="testimonial-text">{testimonials.text}</p>
+              <div className="testimonial-author">
+                <div className="author-info">
+                  <h4>{testimonials.user}</h4>
+                  <div className="rating">
+                    <i className="fa fa-star"></i>
+                    <i className="fa fa-star"></i>
+                    <i className="fa fa-star"></i>
+                    <i className="fa fa-star"></i>
+                    <i className="fa fa-star"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )
       })
     }
 
     return (
-      <section id="testimonials">
-      <div className="text-container">
-         <div className="row">
-
-            <div className="two columns header-col">
-               <h1><span>Client Testimonials</span></h1>
-            </div>
-
-            <div className="ten columns flex-container">
-                  <ul className="slides">
-                      {testimonials}
-                  </ul>
-               </div>
-            </div>
-         </div>
-   </section>
+      <section id="testimonials" className="testimonials-section">
+        <div className="container">
+          <div className="section-header">
+            <h2 className='testimonial-heading'>What People Say</h2>
+            <p className='testimonial-description' style={{color: '#f4eeee'}}>Hear from colleagues and clients about their experience working with me</p>
+          </div>
+          
+          <div className="testimonials-grid">
+            {testimonials}
+          </div>
+          
+          <div className="testimonials-cta">
+            <p>Ready to work together?</p>
+            <a href="#contact" className="button primary-button">
+              Let's Start a Project
+            </a>
+          </div>
+        </div>
+      </section>
     );
   }
 }
