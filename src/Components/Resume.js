7286c6d6359268569164675010938598e073d596
Damiano Chintala <damianochintala@gmail.com>
2025-08-21 11:35:09 +0200

    Update Resume.js with modern layout, certifications section, and improved work experience display

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -6,72 +6,118 @@ class Resume extends Component {
     if(this.props.data){
       var skillmessage = this.props.data.skillmessage;
       var education = this.props.data.education.map(function(education){
-        return <div key={education.school}><h3>{education.school}</h3>
-        <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
-        <p>{education.description}</p></div>
+        return (
+          <div key={education.school} className="resume-item">
+            <div className="resume-item-header">
+              <h3>{education.school}</h3>
+              <span className="resume-item-date">{education.graduated}</span>
+            </div>
+            <h4 className="resume-item-title">{education.degree}</h4>
+            <p className="resume-item-description">{education.description}</p>
+          </div>
+        )
       })
       var work = this.props.data.work.map(function(work){
-        return <div key={work.company}><h3>{work.company}</h3>
-            <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
-            <p>{work.description}</p>
-        </div>
+        return (
+          <div key={work.company} className="resume-item">
+            <div className="resume-item-header">
+              <h3>{work.company}</h3>
+              <span className="resume-item-date">{work.years}</span>
+            </div>
+            <h4 className="resume-item-title">{work.title}</h4>
+            <p className="resume-item-description">{work.description}</p>
+          </div>
+        )
       })
-      var skills = this.props.data.skills.map(function(skills){
-        var className = 'bar-expand '+skills.name.toLowerCase();
-        return <li key={skills.name}><span style={{width:skills.level}}className={className}></span><em>{skills.name}</em></li>
+      var skills = this.props.data.skills.map(function(skill){
+        return (
+          <div key={skill.name} className="skill-item">
+            <div className="skill-info">
+              <span className="skill-name">{skill.name}</span>
+              <span className="skill-level">{skill.level}</span>
+            </div>
+            <div className="skill-bar">
+              <div className="skill-progress" style={{width: skill.level}}></div>
+            </div>
+          </div>
+        )
       })
+      
+      var certifications = this.props.data.certifications ? this.props.data.certifications.map(function(certification){
+        return (
+          <div key={certification.name} className="certification-item">
+            <div className="certification-header">
+              <h4>{certification.name}</h4>
+              <span className="certification-category">{certification.category}</span>
+            </div>
+          </div>
+        )
+      }) : null;
     }
 
     return (
-      <section id="resume">
+      <section id="resume" className="resume-section">
+        <div className="container">
+          <div className="section-header">
+            <h2>Resume</h2>
+            <p>{skillmessage}</p>
+          </div>
 
-      <div className="row education">
-         <div className="three columns header-col">
-            <h1><span>Education</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-            <div className="row item">
-               <div className="twelve columns">
-                 {education}
-               </div>
+          <div className="resume-content">
+            {/* Education Section */}
+            <div className="resume-section-group">
+              <div className="resume-section-header">
+                <div className="section-icon">
+                  <i className="fa fa-graduation-cap"></i>
+                </div>
+                <h3>Education</h3>
+              </div>
+              <div className="resume-items">
+                {education}
+              </div>
             </div>
-         </div>
-      </div>
 
+            {/* Work Experience Section */}
+            <div className="resume-section-group">
+              <div className="resume-section-header">
+                <div className="section-icon">
+                  <i className="fa fa-briefcase"></i>
+                </div>
+                <h3>Work Experience</h3>
+              </div>
+              <div className="resume-items">
+                {work}
+              </div>
+            </div>
 
-      <div className="row work">
+            {/* Skills Section */}
+            <div className="resume-section-group">
+              <div className="resume-section-header">
+                <div className="section-icon">
+                  <i className="fa fa-code"></i>
+                </div>
+                <h3>Skills & Expertise</h3>
+              </div>
+              <div className="skills-container">
+                {skills}
+              </div>
+            </div>
 
-         <div className="three columns header-col">
-            <h1><span>Work</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-          {work}
+            {/* Certifications Section */}
+            <div className="resume-section-group">
+              <div className="resume-section-header">
+                <div className="section-icon">
+                  <i className="fa fa-certificate"></i>
+                </div>
+                <h3>Certifications</h3>
+              </div>
+              <div className="certifications-container">
+                {certifications}
+              </div>
+            </div>
+          </div>
         </div>
-    </div>
-
-
-
-      <div className="row skill">
-
-         <div className="three columns header-col">
-            <h1><span>Skills</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-
-            <p>{skillmessage}
-            </p>
-
-				<div className="bars">
-				   <ul className="skills">
-					  {skills}
-					</ul>
-				</div>
-			</div>
-      </div>
-   </section>
+      </section>
     );
   }
 }
